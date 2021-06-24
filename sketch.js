71e109f3efecd33d1d1ea95e2c5e3770eafb3d99
SHUBHAM SUDAM MHASKE <85775945+Shubham392007@users.noreply.github.com>
2021-06-24 19:54:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var sea,seaImage;
+var ship,shipImage;
+
+function preload(){
+  shipImage=loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+  seaImage=loadImage("sea.png")
+}
+function setup(){
+  createCanvas(400,400);
+
+ sea=createSprite(400,200);
+  sea.addImage(seaImage)
+  sea.velocityX=-5
+ sea.scale=0.3
+  
+  ship=createSprite(200,195,99,35,)
+ ship.addAnimation("movingship",shipImage) 
+ ship.scale=0.3
+ 
+ 
+}
+
+function draw() {
+  background("blue");
+  sea.velocityX=-3
+
+
+if(sea.x<0){
+  sea.x=sea.width/8
+ }
+
+ fill("white")
+ textSize(30);
+ text("SHUBHAM",230,365);
+
+drawSprites();
+}
